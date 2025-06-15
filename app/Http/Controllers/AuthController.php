709ebf3035d57679e8035b09378ea88b2f5598ef
Wesley Moura <wesleymoura.dev@gmail.com>
2025-06-14 22:36:39 -0300

    001

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+
+        return view('login');
+    }
+
+    public function logout(){
+
+        echo 'logout';
+    }
+
+        public function loginSubmit(Request $request){
+
+        echo 'loginSubmit';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::get('/logout', [AuthController::class, 'logout']);
